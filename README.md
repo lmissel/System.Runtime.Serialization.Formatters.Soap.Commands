84c7e521adea3e156438779df901d63c9612b1a2
lmissel <39304706+lmissel@users.noreply.github.com>
2019-08-12 02:40:46 +0200

    New Module and Readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # System.Runtime.Serialization.Formatters.Soap.Commands
+ 
  Dieses PowerShell Module enthält Funktionen mit deren Hilfe Objekte im Soap-Format serialisiert und deserialisiert werden können.
+
+## Voraussetzungen
+
+Um dieses Module nutzen zu können, ist .Net Standard notwendig. Dies sollte bei Windows Systemen standardmäßig zur Verfügung stehen.
+
+## Installation
+
+Kopieren Sie das Module in eins der PowerShell Module Pfade.
+
+## Verwendung
+
+In diesem Beispiel wird gezeigt und erläutert, wie das Module verwendet werden kann.
+
+```powershell
+# Importieren des Moduls
+Import-Module System.Runtime.Serialization.Formatters.Soap.Commands
+
+# Pfad angegeben
+$TargetPath = "C:\test.soap"
+
+# Serialisiert das Objekt
+ConvertTo-SoapFile -File $TargetPath -InputObject $host
+
+# Deserialisiert die angegebene Datei.
+ConvertFrom-SoapFile -File $TargetPath
+
+```
+
+## Hinweis
+
+Das Module verwendet Klassen des Namepaces [System.Runtime.Serialization.Formatters.Soap], mit denen die Umsetzung vereinfacht wird.
---
+++ System.Runtime.Serialization.Formatters.Soap.Commands.psm1
@@ -0,0 +1,78 @@
+﻿#-------------------------------------
+#
+# Module: System.Runtime.Serialization.Formatters.Soap.Commands
+#
+# Description: 
+# Dieses PowerShell Module stellt Funktionen zur Verfügung mit der Hilfe 
+# Objekte im Soap-Format serialisiert und deserialisiert werden können.
+#
+# Namespace: System.Runtime.Serialization.Formatters.Soap
+# Assemblies: System.Runtime.Serialization.Formatters.Soap.dll
+#
+#-------------------------------------
+
+Add-Type -AssemblyName System.Runtime.Serialization.Formatters.Soap
+
+# Deserialisiert den angegebenen Stream in ein Objektdiagramm.
+function ConvertFrom-SoapFile
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # Hilfebeschreibung zu Param1
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String] $File
+    )
+
+    Begin
+    {
+        if (-not(Test-Path -Path $File)) { throw "File not found."}
+    }
+    Process
+    {
+        [System.IO.FileStream] $FileStream = [System.IO.FileStream]::new($File, [System.IO.FileMode]::Open)
+        [System.Runtime.Serialization.Formatters.Soap.SoapFormatter] $SoapFormatter = [System.Runtime.Serialization.Formatters.Soap.SoapFormatter]::new()
+        $OutputObject = $SoapFormatter.Deserialize($FileStream)
+        return $OutputObject
+    }
+    End
+    {
+        $FileStream.Close()
+    }
+}
+
+# Serialisiert das Objekt oder das Objektdiagramm mit dem angegebenen obersten Element (Stamm) in den angegebenen Stream.
+function ConvertTo-SoapFile
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # Hilfebeschreibung zu Param1
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String] $File,
+
+        # Hilfebeschreibung zu Param2
+        [System.Object]
+        $InputObject
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        [System.IO.FileStream] $FileStream = [System.IO.FileStream]::new($File, [System.IO.FileMode]::Create)
+        [System.Runtime.Serialization.Formatters.Soap.SoapFormatter] $SoapFormatter = [System.Runtime.Serialization.Formatters.Soap.SoapFormatter]::new()
+        $SoapFormatter.Serialize($FileStream, $InputObject)
+    }
+    End
+    {
+        $FileStream.Close()
+    }
+}
